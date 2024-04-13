287263534fe2b853eba6a2b54a2bc39ac34c9fd8
Samuel Soto <zemliu_960712@hotmail.com>
2024-04-13 16:02:12 -0600

    packs to view lists, later change doc names

---
+++ Cal.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+
+public class Cal {
+private static Lista lista = new Lista(); // Instancia única de Lista
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+       	//Lista uno = new Lista(scanner);
+		String nombre;
+		
+		
+        System.out.print("Ingrese su nombre: ");
+        nombre = scanner.nextLine();  
+
+        while (true) {
+            System.out.println("Hola, " + nombre + ". Elija la operación que desea realizar:");
+            System.out.println("1. crear lista");
+            System.out.println("2. ver lista");
+            System.out.println("3. modificar lista");
+            System.out.println("4. Salir");
+
+            int opcion = scanner.nextInt();
+
+            if (opcion == 4) {
+                System.out.println("Saliendo del programa. ¡Hasta luego, " + nombre + "!");
+                break;
+            }
+            switch (opcion) {
+                case 1:
+                    /*crear lista*/
+					while (true){
+					lista.parametros();
+                    Object[] nuevaLista = lista.crearLista();
+					lista.agregarLista(nuevaLista);				
+					
+					System.out.print("¿Desea realizar otra lista? (Si/No): ");
+					String respuesta = scanner.next().toLowerCase();
+						
+					if (!respuesta.equals("si")) {
+						System.out.println("Saliendo de crear lista.");
+						break;
+						}
+					}
+                    break; 
+                case 2:
+                    /*ver lista*/
+					
+					lista.mostrarListas();
+					//uno.mostrarListas();
+                    break;
+                case 3:
+                    /*modificar lista*/
+                    break;
+                default:
+                    System.out.println("Opción no válida. Por favor, elija una operación válida.");
+                    continue;
+            }
+
+            //System.out.println("El resultado de la operación es: ");
+
+            System.out.print("¿Desea realizar otra operación? (Si/No): ");
+            String respuesta = scanner.next().toLowerCase();
+
+            if (!respuesta.equals("si")) {
+                System.out.println("Saliendo del programa. ¡Hasta luego, " + nombre + "!");
+                break;
+            }
+        }
+
+        scanner.close();
+	}
+}
---
+++ ListaV.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Lista {
+
+    private int elements; //numero de items de la lista creada por el usuario
+    private String name; //nombre de la lista creada por el usuario
+    private Scanner scanner;
+    private Object[][] listOfLists; //lista que guarda las listas creadas por el usuario
+	private String[] listNames; //lista de los nombres
+	private static int numberOfLists = 0; //numero de listas creadas por el usuario
+	
+    public Lista() {
+        this.scanner = new Scanner(System.in);
+        this.numberOfLists = 0;
+    }
+
+    public void parametros() {
+        System.out.println("Crearemos una nueva lista. ");
+
+        System.out.print("Ingrese el nombre de la lista: ");
+        this.name = scanner.nextLine();
+
+        System.out.print("Ingrese el número de elementos para la lista: ");
+        this.elements = scanner.nextInt();
+        scanner.nextLine();
+		
+		crearNombre();
+    }
+//metodo para almacenar el nombre de las listas
+ 	 
+     public void crearNombre() {
+        if (listNames == null) {
+            listNames = new String[1];
+            listNames[0] = name;
+        } else {
+            String[]temp = Arrays.copyOf(listNames, listNames.length + 1);
+            temp[temp.length - 1] = name;
+            listNames = temp;
+        }
+        
+    } 
+	
+//metodo para que el usuario cree una lista
+    public Object[] crearLista() {
+        Object[] lista = new String[elements];//aqui se crean la lista del usuario
+	
+        for (int i = 0; i < elements; i++) {
+            System.out.print("Ingrese el elemento para la posición " + (i + 1) + " de la lista: ");
+            lista[i] = scanner.nextLine(); //aqui se van llenando las listas
+        }
+        return lista;
+    }
+
+    public void agregarLista(Object[] nuevaLista) {
+        
+		if (listOfLists == null) {
+            listOfLists = new Object[1][];
+            listOfLists[0] = nuevaLista;
+        } else {
+            Object[][] temp = Arrays.copyOf(listOfLists, listOfLists.length + 1);
+            temp[temp.length - 1] = nuevaLista;
+            listOfLists = temp;
+        }
+        numberOfLists++;
+    }
+	
+	public void mostrarListas() {
+        if (listOfLists != null && numberOfLists > 0) {
+            for (int i = 0; i < numberOfLists; i++) {
+                System.out.println("Lista " + (i + 1)+" "+ listNames[i]+" :");
+                for (Object elemento : listOfLists[i]) {
+                    System.out.print(elemento + " ");
+                }
+                System.out.println();
+            }
+        } else {
+            System.out.println("No hay listas creadas aún."+numberOfLists);
+        }
+		
+	 System.out.println(Arrays.deepToString(listOfLists));
+    }
+	
+	
+		
+	
+}
