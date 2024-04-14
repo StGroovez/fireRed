f3ccb3a7ab73e0c60b1972d5a7a6889545455126
Abdo <78060631+StGroovez@users.noreply.github.com>
2024-04-14 12:42:26 -0600

    Versión preliminar de Principal.java
    Se agregó la lógica de crear listas en el Case 1 y se empezó mostrar listas en el Case 2.

--- Principal.java
+++ Principal.java
@@ -1,40 +1,13 @@
 import java.util.Scanner;
-public class Principal {
-    public static void main(String[] args) {
-<<<<<<< HEAD:General.java
-        Scanner scanner = new Scanner(System.in);
+public class Principal {        
     
+    static Lista[] listas = new Lista[1];
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
         System.out.print("Seleccione opción: ");
-       
-    /*public class crearLista {
-    package crearLista;
-    }
-
-    public class crearLista {
-        public String[] lista(String name, int elements, String[] nuevaLista){
-
-            System.out.println("Escribe el número de elementos que va a tener el array");
-
-         String[] listaNueva = new String[elements]; 
-
-            for(int i = 0; i){
-
-            }
-
-
-
-        return nuevaLista;
-        }
-    }*/
 
         while (true) {
-=======
-		
-		Scanner scanner = new Scanner(System.in);
-		ModificarListas Modificar = new ModificarListas(); 
-		
-		while (true) {
->>>>>>> 3548ce0a816d08be3ac10576a79b6699fe062cd1:Principal.java
             System.out.println("Hola, Elija la operación que desea realizar:");
             System.out.println("1. Crear Lista");
             System.out.println("2. Ver Lista");
@@ -50,25 +23,63 @@ public class Principal {
 
             switch (opcion) {
                 case 1:
-<<<<<<< HEAD:General.java
                     /*crearLista = 0;*/
+                    Scanner sc = new Scanner(System.in);
+                    String name;
+                    int num;
+            
+                    for (int i = 0; i < listas.length; i++) {
+                        Lista lista = new Lista();
+            
+                        System.out.println("Nombre de la lista: ");
+                        name = sc.nextLine();
+                        lista.setNombre(name);
+                        
+                        System.out.println("Ingresa un número entre 1 y 25 ");
+                        while(true){
+                            if (sc.hasNextInt()) {
+                                num = sc.nextInt();
+                                sc.nextLine();
+                                lista.setNumeroElementos(num);
+                                break;
+                            }
+                            else{
+                                System.out.println("Debes ingresar un número entero, vuelve a intentarlo: ");
+                                sc.next();
+                            }
+                        }
+                        lista.agregarElementos();
+                        listas[i] = lista;
+                        
+                    }
+                    for (int i = 0; i < listas.length; i++) {
+                        listas[i].mostrarElementos();
+                    }
+
+                    for(int i=0; listas.length > i; i++){
+                        System.out.println("Lista "+ (i+1) + ": " + listas[i].nombre);
+                    }
+                    
+
                     break;
-                case 2:
-                    /*verLista = 0;*/
-                    break;
+                
+		case 2:
+
+                   for(int i=0; listas.length > i; i++){
+                      System.out.println("Lista "+ (i+1) + ": " + listas[i].nombre);
+                    }
+                                           
+                    /*System.out.print("¿Desea realizar otra acción? (Si/No): ");
+                    String respuesta = scanner.next().toLowerCase();
+                    
+                    if (!respuesta.equals("no")) {
+                        for (int i = 0; i < listas.length; i++) {
+                            listas[i].mostrarElementos();
+                        }
+                    }
+                     */
                 case 3:
                     /*modificarLista = 0;*/
-=======
-                    System.out.println("1");
-                    break;
-                case 2:
-                    System.out.println("2");
-                    break;
-                case 3:
-                    System.out.println("3");
-					Modificar.mostrarListas();
-					
->>>>>>> 3548ce0a816d08be3ac10576a79b6699fe062cd1:Principal.java
                     break;
                 default:
                     System.out.println("Opción no válida. Por favor, elija una acción válida.");
@@ -79,14 +90,9 @@ public class Principal {
             String respuesta = scanner.next().toLowerCase();
 
             if (!respuesta.equals("si")) {
-                System.out.println("Saliendo del programa. ¡Hasta luego, !");
+                System.out.println("Saliendo del programa. ¡Hasta luego!");
                 break;
             }
         }
-<<<<<<< HEAD:General.java
-
     }
-=======
-	}
->>>>>>> 3548ce0a816d08be3ac10576a79b6699fe062cd1:Principal.java
 }
