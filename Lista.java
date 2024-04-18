3382b110220555b59778e0ea5451d73c3920ca72
Abdo <78060631+StGroovez@users.noreply.github.com>
2024-04-18 16:55:02 -0600

    Update Lista.java

--- Lista.java
+++ Lista.java
@@ -1,33 +1,29 @@
 import java.util.Scanner;
-public class Lista {
-    /* ATRIBUTOS DE LA LISTA */
 
+public class Lista {
+    /*    ATRIBUTOS DE LA LISTA    */
     String[] elementos = {};
     protected String nombre;
     protected int numeroElementos;
     static Scanner sc = new Scanner(System.in);
 
-    /* MÉTODOS DE LA LISTA */
-
+    /*    MÉTODOS DE LA LISTA    */
     public void mostrarElementos() {    
         for (int i = 0; i < elementos.length; i++) {
             System.out.println((i+1) + " " + getElementos()[i]);
         }    
     }
-
-    /* SETTEA EL NOMBRE */
+    /*    SETTEA EL NOMBRE    */
     public void setNombre(String nombre){
         this.nombre = nombre;
         System.out.println("El nombre de la lista es: " + this.nombre);
     }
-
-    /* SETTEA EL NÚMERO DE ELEMENTOS */
+    /*    SETTEA EL NÚMERO DE ELEMENTOS    */
     public void setNumeroElementos(int numeroElementos){  
         this.elementos = new String[numeroElementos];
         System.out.println("El número de elementos de la lista es: " + elementos.length);
-    }
-    
-    /* AGREGA ELEMENTOS A LA LISTA */
+    }    
+    /*    AGREGA ELEMENTOS A LA LISTA    */
     public void agregarElementos(){
         for (int i = 0; i < elementos.length; i++) {     
             System.out.println("Agrega un elemento: ");
@@ -40,17 +36,14 @@ public class Lista {
         }
         System.out.println("Has llegado al límite de elementos, tu lista ha sido creada");
     }
-
-    /* METODO CONSTRUCTOR */
-
+    /*    MÉTODO CONSTRUCTOR     */
     public Lista(String name, int numElements){
         setNombre(name);
         setNumeroElementos(numElements);
         agregarElementos();
     }
 
-    /*validaciones */
-
+    /*    VALIDACIONES     */
     static String validarNombre(){
         String nombre = sc.nextLine();
         while (nombre == null || nombre.isEmpty()) {
@@ -111,9 +104,7 @@ public class Lista {
         }
         return numero;
     }
-
     /* MODIFICAR LISTAS */
-
     public String[] getElementos() {
         return this.elementos;
     }
